ce3a77246d6155d994c7abed568d58ac9124b552
BugHunter-X <askzenai@gmail.com>
2024-12-03 06:38:36 +0100

    Add firebaseBug.js file

---
+++ firebaseBug.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+// other imports
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  // ... other config values
+};
+
+const app = initializeApp(firebaseConfig);
+
+// ... rest of your Firebase code
